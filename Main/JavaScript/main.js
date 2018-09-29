d9046b8adb1ec16465ce5a4ec49fd4a9c0536765
Pedro Emilio Perez Franco <pit.upgop.iti@gmail.com>
2018-09-29 12:08:40 -0500

    Commit inicial

---
+++ Main/JavaScript/main.js
@@ -0,0 +1,11 @@
+let $ = require('jquery')
+let fs = require('fs')
+let filename1 = 'encontrados';
+let filename2 = 'buscados';
+
+let encontrados = 0;
+let buscados = 0;
+
+function agregarContadores(found,toFind){
+    
+}
