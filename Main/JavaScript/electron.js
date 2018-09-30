00a667921347c99d9006001112bd0a1ea970fa53
Pedro Emilio Perez Franco <pit.upgop.iti@gmail.com>
2018-09-30 07:32:46 -0500

    Commit final
    Proyecto terminado al 90%

--- Main/JavaScript/electron.js
+++ Main/JavaScript/electron.js
@@ -5,7 +5,7 @@ const path = require('path')
 let win  
 
 function createWindow() { 
-   win = new BrowserWindow({width: 800, height: 600}) 
+   win = new BrowserWindow({width: 600, height: 450, transparent: true, frame: false, title:false}) 
    win.loadURL(url.format ({ 
       pathname: path.join(__dirname, '../views/index.html'), 
       protocol: 'file:', 
